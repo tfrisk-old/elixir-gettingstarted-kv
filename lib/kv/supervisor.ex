55cf3762302ef1f823e17d39305e561b91030272
Teemu Frisk <teemu.frisk@gmail.com>
2015-02-18 15:53:06 +0200

    Finish supervisor

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -7,13 +7,36 @@ defmodule KV.Supervisor do
 
   @manager_name KV.EventManager
   @registry_name KV.Registry
+  @bucket_sup_name KV.Bucket.Supervisor
 
   def init(:ok) do
     children = [
       worker(GenEvent, [[name: @manager_name]]),
-      worker(KV.Registry, [@manager_name, [name: @registry_name]])
+      supervisor(KV.Bucket.Supervisor, [[name: @bucket_sup_name]]),
+      worker(KV.Registry, [@manager_name, @bucket_sup_name,
+        [name: @registry_name]])
     ]
 
     supervise(children, strategy: :one_for_one)
+  end
+end
+
+defmodule KV.Bucket.Supervisor do
+  use Supervisor
+
+  def start_link(opts \\ []) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  def start_bucket(supervisor) do
+    Supervisor.start_child(supervisor, [])
+  end
+
+  def init(:ok) do
+    children = [
+      worker(KV.Bucket, [], restart: :temporary)
+    ]
+
+    supervise(children, strategy: :simple_one_for_one)
   end
 end
--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -11,8 +11,9 @@ defmodule KV.RegistryTest do
   end
 
   setup do
+    {:ok, sup} = KV.Bucket.Supervisor.start_link
     {:ok, manager} = GenEvent.start_link
-    {:ok, registry} = KV.Registry.start_link(manager)
+    {:ok, registry} = KV.Registry.start_link(manager, sup)
 
     GenEvent.add_mon_handler(manager, Forwarder, self())
     {:ok, registry: registry}
@@ -32,6 +33,15 @@ defmodule KV.RegistryTest do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "removes bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    # Kill the bucket and wait for the notification
+    Process.exit(bucket, :shutdown)
+    assert_receive {:exit, "shopping", ^bucket}
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
